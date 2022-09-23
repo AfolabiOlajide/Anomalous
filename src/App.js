6d2741bf51caf321bc00002fab19e7f190c8362f
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-09-23 02:31:36 +0100

    cleaned app.js file

--- src/App.js
+++ src/App.js
@@ -2,44 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import "./app.scss";
 import "./index.scss";
-// pages import
-import HomePage from "./pages/home";
-import MarketPlace from "./pages/marketPlace";
-import Blog from "./pages/blog";
-import Contact from "./pages/contact";
-import Login from "./pages/login";
-import SignUp from "./pages/signUp";
-import WishList from './pages/wishlist';
-import Cart from './pages/cart'
-import Profile from "./pages/profile";
-import About from "./pages/about"
-// components imports
-import NavBar from "./components/navbar/NavBar";
-import Footer from "./components/footer/Footer";
-import TopBar from "./components/topbar/TopBar";
-import NewsLetter from "./components/newsletter/NewsLetter";
+
+import User from "./user";
+import Admin from "./admin";
 
 function App() {
 	return (
 		<div className="app">
-			<TopBar />
-			<NavBar />
-			<main>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/marketplace" element={<MarketPlace />} />
-					<Route path="/blog" element={<Blog />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/signup" element={<SignUp />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="/wishlist" element={<WishList />} />
-					<Route path="/profile" element={<Profile />} />
-				</Routes>
-			</main>
-			<NewsLetter />
-			<Footer />
+			<Routes>
+				<Route path="*" element={<User />} />
+				<Route path="/admin" element={<Admin />} />
+			</Routes>
 		</div>
 	);
 }
---
+++ src/user.js
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./app.scss";
+import "./index.scss";
+// pages import
+import HomePage from "./pages/home";
+import MarketPlace from "./pages/marketPlace";
+import Blog from "./pages/blog";
+import Contact from "./pages/contact";
+import Login from "./pages/login";
+import SignUp from "./pages/signUp";
+import WishList from './pages/wishlist';
+import Cart from './pages/cart'
+import Profile from "./pages/profile";
+import About from "./pages/about"
+// components imports
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/Footer";
+import TopBar from "./components/topbar/TopBar";
+import NewsLetter from "./components/newsletter/NewsLetter";
+
+function User() {
+	return (
+		<div className="user">
+			<TopBar />
+			<NavBar />
+			<main>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/marketplace" element={<MarketPlace />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/wishlist" element={<WishList />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
+			</main>
+			<NewsLetter />
+			<Footer />
+		</div>
+	);
+}
+
+export default User;
