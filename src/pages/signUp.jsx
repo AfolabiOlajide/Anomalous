810ae80d86609d8ca76e1663e4a924607b481ccd
Afolabi Olajide Samuel <samolajide20@gmail.com>
2022-09-28 00:30:27 +0100

    Sign up page built with mobile responsiveness

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,6 +1,28 @@
+import { Button } from "@mui/material";
+import React from "react";
+import Input from "../ui/input/input";
+
+import "./signUp.scss";
+
 const SignUp = () => {
     return (
-        <h1>SignUp Page</h1>
+        <section className="signup">
+            <div className="design"></div>
+            <div className="form-place">
+                <h1>Create an account</h1>
+                <p>Let's get started with creating your account</p>
+                <form action="">
+                    <Input name="Name" type="text" placeholder="Enter your name"  />
+                    <Input name="Email" type="email" placeholder="Enter your email"  />
+                    <Input name="Password" type="password" placeholder="Enter your password"  />
+                    <Input name="Confirm Password" type="password" placeholder="Confirm your password"  />
+                </form>
+                <div className="cta">
+                    <Button variant="contained">Sign Up</Button>
+                    <Button variant="outlined">Signin with Google</Button>
+                </div>
+            </div>
+        </section>
     )
 }
 
